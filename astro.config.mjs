1fd3cd186f964c2aa064cdb65af399a119c167ce
ArishaMak <makovchikarina@icloud.com>
2026-03-25 15:08:29 +0300

    fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
-
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: 'https://ArishaMak.github.io',
-
+    site: 'https://ArishaMak.github.io/',
     base: '/portfolio',
 
     server: {

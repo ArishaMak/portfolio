893d328d968235f8daad127000a50e3c8a0f4329
ArishaMak <makovchikarina@icloud.com>
2026-04-01 22:24:40 +0300

    продакшн ёмаё

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
     site: 'https://arishasmakportfolio.netlify.app',
     base: '/',
+
+    integrations: [sitemap()],
+
+    image: {
+        domains: ['cdn.jsdelivr.net', 'images.unsplash.com']
+    },
 
     server: {
         port: 3000,
@@ -12,6 +19,6 @@ export default defineConfig({
     vite: {
         css: {
             devSourcemap: true,
-        },
-    },
+        }
+    }
 });
